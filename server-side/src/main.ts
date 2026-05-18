67873cb3bdaa134e7ed102f6a543f9d01b24309f
popochoo <bestekst0@gmail.com>
2026-05-18 14:23:38 +0300

    modification main.ts

--- server-side/src/main.ts
+++ server-side/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+import cookieParser from 'cookie-parser'
+
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(process.env.PORT ?? 3000)
+
+	app.use(cookieParser())
+	app.enableCors({
+		origin: [process.env.CLIENT_URL],
+		credentials: true,
+		epxosedHeaders: ['set-cookie']
+	})
+
+	await app.listen(5000)
 }
-bootstrap()
+bootstrap().catch(err => {
+	console.error('Application failed to start:', err)
+	process.exit(1)
+})
