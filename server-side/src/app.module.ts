ab11a1e6a9dcc50cdd102c1f47751619c2f382e0
popochoo <bestekst0@gmail.com>
2026-05-21 01:37:21 +0300

    add services in app.module.ts

--- server-side/src/app.module.ts
+++ server-side/src/app.module.ts
@@ -3,8 +3,22 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
+import { CategoryModule } from './category/category.module'
+import { FileModule } from './file/file.module'
+import { OrderModule } from './order/order.module'
+import { BookModule } from './book/book.module'
+import { ReviewModule } from './review/review.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule]
+	imports: [
+		ConfigModule.forRoot(),
+		AuthModule,
+		UserModule,
+		CategoryModule,
+		FileModule,
+		OrderModule,
+		BookModule,
+		ReviewModule
+	]
 })
 export class AppModule {}
