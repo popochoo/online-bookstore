36dece537ae036d21da247fcdea102fb1941c5ac
popochoo <bestekst0@gmail.com>
2026-05-21 20:22:14 +0300

    prettier fix

--- client-side/src/config/url.config.ts
+++ client-side/src/config/url.config.ts
@@ -1,19 +1,19 @@
 export const APP_URL = process.env.APP_URL as string
 
 export const PUBLIC_URL = {
-    root: (url = '') => `${url ? url : ''}`,
+	root: (url = '') => `${url ? url : ''}`,
 
-    home: () => PUBLIC_URL.root('/'),
-    auth: () => PUBLIC_URL.root('/auth'),
-    explorer: (query = '') => PUBLIC_URL.root(`/explorer${query}`),
+	home: () => PUBLIC_URL.root('/'),
+	auth: () => PUBLIC_URL.root('/auth'),
+	explorer: (query = '') => PUBLIC_URL.root(`/explorer${query}`),
 
-    books: (id = '') => PUBLIC_URL.root(`/book/${id}`),
-    categories: (id = '') => PUBLIC_URL.root(`/category/${id}`),
+	books: (id = '') => PUBLIC_URL.root(`/book/${id}`),
+	categories: (id = '') => PUBLIC_URL.root(`/category/${id}`)
 }
 
 export const DASHBOARD_URL = {
-    root: (url = '') => `/dashboard${url ? url : ''}`,
+	root: (url = '') => `/dashboard${url ? url : ''}`,
 
-    home: () => DASHBOARD_URL.root('/'),
-    favotires: () => DASHBOARD_URL.root('/favotires'),
+	home: () => DASHBOARD_URL.root('/'),
+	favotires: () => DASHBOARD_URL.root('/favotires')
 }
--- client-side/src/constants/seo.constants.ts
+++ client-side/src/constants/seo.constants.ts
@@ -1,10 +1,11 @@
 export const NO_INDEX_PAGE = {
-    robots: {
-        index: false,
-        follow: false
-    }
+	robots: {
+		index: false,
+		follow: false
+	}
 }
 
 export const SITE_NAME = 'BookStore'
 
-export const SITE_DESCRIPTION = 'Ваш гид в мире книг. Удобный поиск, отзывы читателей и быстрая покупка онлайн.'
+export const SITE_DESCRIPTION =
+	'Ваш гид в мире книг. Удобный поиск, отзывы читателей и быстрая покупка онлайн.'
