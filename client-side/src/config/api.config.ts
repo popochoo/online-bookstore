6e6a395a391ec3a3d5b0a7d4c3b79934cef66094
popochoo <bestekst0@gmail.com>
2026-05-21 18:15:09 +0300

    create a api.config.ts and url.config.ts

---
+++ client-side/src/config/api.config.ts
@@ -0,0 +1,13 @@
+export const SERVER_URL = process.env.SERVER_URL as string
+
+export const API_URL = {
+    root: (url = '') => `${url ? url : ''}`,
+
+    auth: (url = '') => API_URL.root(`/auth${url}`),
+    users: (url = '') => API_URL.root(`/users${url}`),
+    books: (url = '') => API_URL.root(`/books${url}`),
+    categories: (url = '') => API_URL.root(`/categories${url}`),
+    reviews: (url = '') => API_URL.root(`/reviews${url}`),
+    orders: (url = '') => API_URL.root(`/orders${url}`),
+    files: (url = '') => API_URL.root(`/files${url}`),
+}
