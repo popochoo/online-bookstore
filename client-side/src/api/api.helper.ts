8547ea1417f5e07622282327f50942893e46afbd
popochoo <bestekst0@gmail.com>
2026-05-21 18:37:54 +0300

    create a axios interceptors

---
+++ client-side/src/api/api.helper.ts
@@ -0,0 +1,12 @@
+export const getContentType = () => ({
+    'Content-type': 'application/json'
+})
+
+// Создать интерфейс для ошибок Axios
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const errorCatch = (error: any): string => {
+    const message = error?.response?.data?.message
+
+    return message ? typeof error.response.data.message === 'object' ? message[0] : message : error.message
+}
