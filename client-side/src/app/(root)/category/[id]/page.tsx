dafd7830c446ab631de6a797b9b076515f002c51
popochoo <bestekst0@gmail.com>
2026-05-25 23:28:06 +0300

    create category page

---
+++ client-side/src/app/(root)/category/[id]/page.tsx
@@ -0,0 +1,58 @@
+import { Metadata } from 'next'
+import { unstable_noStore } from 'next/cache'
+
+import { Catalog } from '@/src/components/ui/catalog/Catalog'
+import { bookService } from '@/src/services/book.service'
+import { categoryService } from '@/src/services/category.service'
+
+//export const revalidate = 0 // 60
+export const dynamic = 'force-dynamic'
+
+interface CategoryPageProps {
+	params: Promise<{ id: string }>
+}
+
+async function getBooks(id: string) {
+	unstable_noStore()
+	console.log('ЗАПРОШЕННЫЙ ID КАТЕГОРИИ:', id)
+	const books = await bookService.getByCategory(id)
+
+	const category = await categoryService.getById(id)
+
+	return { books, category }
+}
+
+export async function generateMetadata({
+	params
+}: CategoryPageProps): Promise<Metadata> {
+	const { books, category } = await getBooks((await params).id)
+
+	return {
+		title: category.title,
+		description: category.description,
+		openGraph: {
+			images: [
+				{
+					url: books[0].images[0],
+					width: 1000,
+					height: 1000,
+					alt: category.title
+				}
+			]
+		}
+	}
+}
+
+export default async function CategoryPage({ params }: CategoryPageProps) {
+	const { books, category } = await getBooks((await params).id)
+
+	return (
+		<div className='my-6'>
+			<Catalog
+				title={category.title}
+				description={category.description}
+				books={books}
+			/>
+		</div>
+	)
+}
