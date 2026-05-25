7d275204acc55596a04b48d49b21d9ac90121a93
popochoo <bestekst0@gmail.com>
2026-05-25 18:32:55 +0300

    update Home.tsx

--- client-side/src/app/(root)/Home.tsx
+++ client-side/src/app/(root)/Home.tsx
@@ -1,4 +1,6 @@
 import { Hero } from './hero/Hero'
+import { Catalog } from '@/src/components/ui/catalog/Catalog'
+import { PUBLIC_URL } from '@/src/config/url.config'
 import { IBook } from '@/src/shared/types/book.interface'
 
 interface HomeProps {
@@ -9,6 +11,13 @@ export function Home({ books }: HomeProps) {
 	return (
 		<>
 			<Hero />
+			<Catalog
+				title='Хиты продаж'
+				description='Самые попурядрные книги нашего магазина.'
+				linkTitle='Узнать больше'
+				link={PUBLIC_URL.explorer()}
+				books={books}
+			/>
 		</>
 	)
 }
