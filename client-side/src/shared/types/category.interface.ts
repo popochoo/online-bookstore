b1e8bef73ff6d726e7ba7ea64d5b47136fbe7774
popochoo <bestekst0@gmail.com>
2026-05-25 18:34:13 +0300

    Fix category.interface.ts

--- client-side/src/shared/types/category.interface.ts
+++ client-side/src/shared/types/category.interface.ts
@@ -1,10 +1,11 @@
 export interface ICategory {
 	id: string
 	createdAt: string
-	name: string
-	value: string
+	title: string
+	description: string
 }
 
-export type ICategoryInput = Pick<ICategory, 'name' | 'value'>
+export type ICategoryInput = Pick<ICategory, 'title' | 'description'>
 
 //export interface ICategoryInput extends Pick<ICategory, 'name' | 'value'> {}
+// 7:19:23
