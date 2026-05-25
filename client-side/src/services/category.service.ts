934800abb644db404898cc5bdfce268ec0c99272
popochoo <bestekst0@gmail.com>
2026-05-25 23:29:59 +0300

    fix category.service.ts

--- client-side/src/services/category.service.ts
+++ client-side/src/services/category.service.ts
@@ -13,7 +13,7 @@ class CategoryService {
     }
 
     async getById(id: string) {
-        const { data } = await axiosClassic<ICategory[]>({
+        const { data } = await axiosClassic<ICategory>({
             url: API_URL.categories(`/by-id/${id}`),
             method: 'GET'
         })
